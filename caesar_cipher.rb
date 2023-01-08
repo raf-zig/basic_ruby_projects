fd62352a9089b4c89323407b63c0ca427e5c7daf
raf-zig <ziravin@yandex.ru>
2023-01-08 22:11:34 +0500

    change each method

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -4,13 +4,13 @@ def ceaser_cipher(string, key)
   resulting_array = []
     alphabet = ("a".."z").to_a
     entrance_array = string.downcase.split("")
-    entrance_array.each {|x| 
+    entrance_array.each do |x| 
         if alphabet.include?(x)
           resulting_array << alphabet[alphabet.index(x) - 26 + key]
         else
           resulting_array << x
         end
-    }
+    end
     resulting_array.join
 end
 print ceaser_cipher('She looked around for another entrance but saw none', 3)

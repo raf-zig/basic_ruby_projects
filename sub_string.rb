97d644291716eacaf500bff5db8026c9c9c7acab
raf-zig <ziravin@yandex.ru>
2023-01-11 23:02:46 +0500

    add the stock_picker method

--- sub_string.rb
+++ sub_string.rb
@@ -1,3 +1,5 @@
+#The method #substrings that takes a word as the first argument and then an array of valid substrings as the second argument.
+# It should return a hash listing search substring that was found in the original string and how many times it was found.
 dictionary = ["mediocre","below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 def substring (str, dictionary)
   hash = {}

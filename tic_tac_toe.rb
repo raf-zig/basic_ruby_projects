c998f1307953f2e5393bd65be7f20b827e245585
Ravil Ziganshin <84528349+raf-zig@users.noreply.github.com>
2023-01-23 10:38:39 +0500

    add  Game Class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -11,3 +11,23 @@ Class Player
     chosen_position = gets.chomp.to_i
   end
 end
+
+Class Game
+  WIN_COMBINATIONS = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9]
+  ]
+
+  def initialize
+    @players = [Player.new('Player_1', 'x'), Player.new('Player_2', '0')]
+    @current_player = @players[0]
+    @board = %w[1 2 3 4 5 6 7 8 9]
+  end
+  
+end

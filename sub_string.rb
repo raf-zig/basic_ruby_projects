257e3d059d4f9d38b4498a31eab97ffa36ffc47e
Ravil Ziganshin <84528349+raf-zig@users.noreply.github.com>
2023-02-08 19:43:21 +0500

    Update sub_string.rb

--- sub_string.rb
+++ sub_string.rb
@@ -1,7 +1,7 @@
 #The method "substring" that takes a word as the first argument and then an array of valid substrings as the second argument.
 # It should return a hash listing search substring that was found in the original string and how many times it was found.
-dictionary = ["mediocre","below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-def substring (str, dictionary)
+dictionary = ["mediocre", "below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+def substring(str, dictionary)
   hash = {}
   dictionary.each do |word|
     if str.downcase.include?(word)

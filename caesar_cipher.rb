92dc2786ff24bc30a8a8ad5e8232e5ce0cd91af5
Ravil Ziganshin <84528349+raf-zig@users.noreply.github.com>
2023-05-31 00:20:45 +0500

    Update caesar_cipher.rb

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,14 +2,14 @@
 # is replaced by a letter  some fixed number of positions down the alphabet
 def ceaser_cipher(string, key)
   resulting_array = []
-  alphabet = ("a".."z").to_a
-  entrance_array = string.downcase.split("")
-  entrance_array.each do |x| 
-    if alphabet.include?(x)
-      resulting_array << alphabet[alphabet.index(x) - 26 + key]
-    else
-      resulting_array << x
-    end
+  alphabet = ('a'..'z').to_a
+  entrance_array = string.downcase.split('')
+  entrance_array.each do |x|
+    resulting_array << if alphabet.include?(x)
+                         alphabet[alphabet.index(x) - 26 + key]
+                       else
+                         x
+                       end
   end
   resulting_array.join
 end
